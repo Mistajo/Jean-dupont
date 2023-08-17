54a5856dac8538bd598d1b763079930b8c44b953
Mistajo <130221389+Mistajo@users.noreply.github.com>
2023-08-17 23:12:13 +0200

    pas de modification

--- public/test.php
+++ public/test.php
@@ -14,21 +14,25 @@
         if ( $extension !== "jpg" && $extension !== "jpeg" && $extension !== "png" && $extension !== "webp"  ) 
         {
             $errors['image'] = "Le format n'est pas valide.";
+            
         }
     }
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Document</title>
-    </head>
-    <body>
-        <form method="post" enctype="multipart/form-data">
-            <input type="file" name="image">
-            <input type="submit">
-        </form>
-    </body>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <form method="post" enctype="multipart/form-data">
+        <input type="file" name="image">
+        <input type="submit">
+    </form>
+</body>
+
 </html>
